33a1e0cd06810f5119cfd35000df4ebac468f039
Dokaev Deni <dkv.deni@gmail.com>
2025-05-14 19:03:26 +0200

    Update Medecin.py

--- Medecin.py
+++ Medecin.py
@@ -1,8 +1,9 @@
 class Doctor:
-    def __init__(self, first_name, last_name, specialty):
+    def __init__(self, first_name, last_name, specialty, inami,):
         self.first_name = first_name
         self.last_name = last_name
         self.specialty = specialty
+        self.inami = number_inami
 
     def display_info(self):
         print(f"Name: {self.first_name} {self.last_name}")
