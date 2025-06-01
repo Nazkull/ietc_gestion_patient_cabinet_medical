0d9a2cbb8e41c7152c7abc5f826fb25ed403f2c8
Dokaev Deni <dkv.deni@gmail.com>
2025-06-01 21:01:31 +0200

    Update Patient.py

--- Patient.py
+++ Patient.py
@@ -1,19 +1,15 @@
-def dclass Patient:
-    def __init__(self, nom, prenom, age, id_patient, telephone, email):
-        # Constructeur de la classe Patient
-        self.nom = nom
-        self.prenom = prenom
+class Patient:
+    def __init__(self, first_name, last_name, age, patient_id, phone_number, email):
+        #constructeur patient
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
-        self.id_patient = id_patient
-        self.telephone = telephone
+        self.patient_id = patient_id
+        self.phone_number = phone_number
         self.email = email
+#fonction pour afficher les infos du patient
+    def show_info(self):
+        return f"Patient ID: {self.patient_id}\nName: {self.first_name} {self.last_name}\nAge: {self.age} years old\nPhone: {self.phone_number}\nEmail: {self.email}"
 
-    def afficher_infos(self):
-        # Methode pour afficher les informations du patient
-        print(f"Patient ID: {self.id_patient}")
-        print(f"Nom: {self.nom}")
-        print(f"Prénom: {self.prenom}")
-        print(f"Âge: {self.age} ans")
-        print(f"Téléphone: {self.telephone}")
-        print(f"Email: {self.email}")
+
 
